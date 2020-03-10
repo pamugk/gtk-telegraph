4b20a34780c343d77fddb420c67f81fc206eead1
Alexander Permyakov <pamugk@gmail.com>
2020-03-10 15:46:02 +0500

    Updated client GUI

--- src/controller.c
+++ src/controller.c
@@ -1,9 +1,22 @@
+#include <arpa/inet.h>
 #include "controller.h"
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
 
-void initialize()
-{
+User* contacts;
+Group* groups;
+
+void cleanup() {
+	
 }
 
-void cleanup()
-{
+void initialize() {
+	
+}
+
+void sendMessage(long to, char* message) {
 }
--- src/gtk_telegraph.c
+++ src/gtk_telegraph.c
@@ -31,23 +31,28 @@
 
 G_DEFINE_TYPE (Gtktelegraph, gtk_telegraph, GTK_TYPE_APPLICATION);
 
-/* ANJUTA: Macro GTK_TELEGRAPH_APPLICATION gets Gtktelegraph - DO NOT REMOVE */
 struct _GtktelegraphPrivate
 {
-	/* ANJUTA: Widgets declaration for gtk_telegraph.ui - DO NOT REMOVE */
 	GtkStack* drawerStack;
-	GtkWidget* chatsDrawer;
-	GtkWidget* settingsDrawer;
+	GtkBox* chatsDrawer;
+	GtkBox* settingsDrawer;
+	GtkBox* messagesBox;
 	GtkRevealer* infoboxRevealer;
+	GtkWindow* callsDialog;
+	GtkWindow* contactsDialog;
+	GtkWindow* settingsDialog;
+	GtkAboutDialog* aboutDialog;
 };
 
+GtkCssProvider* darkProvider;
+GtkCssProvider* lightProvider;
 GtktelegraphPrivate *priv;
+GdkScreen* screen;
 
 /* Create a new window loading a file */
 static void
 gtk_telegraph_new_window (GApplication *app,
-                           GFile        *file)
-{
+                           GFile        *file){
 	GtkWidget *window;
 
 	GtkBuilder *builder;
@@ -75,20 +80,32 @@ gtk_telegraph_new_window (GApplication *app,
 				UI_FILE);
         }
 	GtkStyleContext* context = gtk_widget_get_style_context (window);
-	GtkCssProvider *provider = gtk_css_provider_new ();
-	gtk_css_provider_load_from_path (provider, "css/main.css", NULL);
-	gtk_style_context_add_provider (context,
+	screen = gtk_style_context_get_screen(context);
+	darkProvider = gtk_css_provider_new ();
+	gtk_css_provider_load_from_path (darkProvider, "css/main_dark.css", NULL);
+	lightProvider = gtk_css_provider_new ();
+	gtk_css_provider_load_from_path (lightProvider, "css/main_light.css", NULL);
+	GtkCssProvider* provider = gtk_css_provider_new ();
+	gtk_css_provider_load_from_path (provider, "css/main_light.css", NULL);
+	gtk_style_context_add_provider_for_screen (screen,
                                     GTK_STYLE_PROVIDER(provider),
-                                    GTK_STYLE_PROVIDER_PRIORITY_THEME);
+                                    GTK_STYLE_PROVIDER_PRIORITY_USER);
+	gtk_style_context_add_provider_for_screen (screen,
+                                    GTK_STYLE_PROVIDER(lightProvider),
+                                    GTK_STYLE_PROVIDER_PRIORITY_USER);
 	
-	/* ANJUTA: Widgets initialization for gtk_telegraph.ui - DO NOT REMOVE */
+
 	priv->drawerStack = GTK_STACK (gtk_builder_get_object(builder, "drawerStack"));
-	priv->chatsDrawer = GTK_WIDGET (gtk_builder_get_object(builder, "chatsDrawer"));
-	priv->settingsDrawer = GTK_WIDGET (gtk_builder_get_object(builder, "settingsDrawer"));
+	priv->chatsDrawer = GTK_BOX (gtk_builder_get_object(builder, "chatsDrawer"));
+	priv->settingsDrawer = GTK_BOX (gtk_builder_get_object(builder, "settingsDrawer"));
+	priv->messagesBox = GTK_BOX (gtk_builder_get_object(builder, "messagesBox"));
 	priv->infoboxRevealer = GTK_REVEALER (gtk_builder_get_object(builder, "infoboxRevealer"));
+	priv->callsDialog = GTK_WINDOW(gtk_builder_get_object(builder, "callsDialog"));
+	priv->contactsDialog = GTK_WINDOW(gtk_builder_get_object(builder, "contactsDialog"));
+	priv->settingsDialog = GTK_WINDOW(gtk_builder_get_object(builder, "settingsDialog"));
+	priv->aboutDialog = GTK_ABOUT_DIALOG(gtk_builder_get_object(builder, "aboutDialog"));
 	
 	g_object_unref (builder);
-	
 	
 	gtk_window_set_application (GTK_WINDOW (window), GTK_APPLICATION (app));
 	if (file != NULL)
@@ -102,8 +119,7 @@ gtk_telegraph_new_window (GApplication *app,
 
 /* GApplication implementation */
 static void
-gtk_telegraph_activate (GApplication *application)
-{
+gtk_telegraph_activate (GApplication *application){
 	gtk_telegraph_new_window (application, NULL);
 }
 
@@ -111,8 +127,7 @@ static void
 gtk_telegraph_open (GApplication  *application,
                      GFile        **files,
                      gint           n_files,
-                     const gchar   *hint)
-{
+                     const gchar   *hint){
 	gint i;
 
 	for (i = 0; i < n_files; i++)
@@ -120,20 +135,17 @@ gtk_telegraph_open (GApplication  *application,
 }
 
 static void
-gtk_telegraph_init (Gtktelegraph *object)
-{
+gtk_telegraph_init (Gtktelegraph *object){
 	object->priv = G_TYPE_INSTANCE_GET_PRIVATE (object, GTK_TELEGRAPH_TYPE_APPLICATION, GtktelegraphPrivate);
 }
 
 static void
-gtk_telegraph_finalize (GObject *object)
-{
+gtk_telegraph_finalize (GObject *object){
 	G_OBJECT_CLASS (gtk_telegraph_parent_class)->finalize (object);
 }
 
 static void
-gtk_telegraph_class_init (GtktelegraphClass *klass)
-{
+gtk_telegraph_class_init (GtktelegraphClass *klass){
 	G_APPLICATION_CLASS (klass)->activate = gtk_telegraph_activate;
 	G_APPLICATION_CLASS (klass)->open = gtk_telegraph_open;
 
@@ -142,30 +154,92 @@ gtk_telegraph_class_init (GtktelegraphClass *klass)
 	G_OBJECT_CLASS (klass)->finalize = gtk_telegraph_finalize;
 }
 
-on_window_destroy(GtkWindow *window)
-{
-	cleanup();
+add_to_contacts_btn_clicked(GtkButton *button){
+	printf("Contact added\n");
 }
 
-drawer_btn_clicked (GtkButton *button)
-{
-	gtk_stack_set_visible_child(priv->drawerStack, priv->settingsDrawer);
+block_user_btn_clicked (GtkButton *button, gpointer user_data){
+	printf("User blocked\n");
 }
 
-bookmark_btn_clicked (GtkButton *button)
-{
+bookmark_btn_clicked (GtkButton *button){
 	gtk_stack_set_visible_child(priv->drawerStack, priv->chatsDrawer);
 }
 
-infopanel_btn_clicked (GtkButton *button)
-{
+calls_btn_clicked(GtkButton *button){
+	gtk_widget_show(priv->callsDialog);
+}
+
+calls_close_btn_clicked(GtkButton *button){
+	gtk_widget_hide(priv->callsDialog);
+}
+
+clear_history_btn_clicked (GtkButton *button, gpointer user_data){
+	printf("History cleared\n");
+}
+
+contacts_btn_clicked(GtkButton *button){
+	gtk_widget_show(priv->contactsDialog);
+}
+
+contacts_close_btn_clicked(GtkButton *button){
+	gtk_widget_hide(priv->contactsDialog);
+}
+
+delete_chat_btn_clicked (GtkButton *button, gpointer user_data){
+	printf("Chat deleted\n");
+}
+
+drawer_btn_clicked (GtkButton *button){
+	gtk_stack_set_visible_child(priv->drawerStack, priv->settingsDrawer);
+}
+
+infopanel_btn_clicked (GtkButton *button){
 	gboolean childRevealed = gtk_revealer_get_reveal_child (priv->infoboxRevealer);
 	gtk_revealer_set_reveal_child (priv->infoboxRevealer, !childRevealed);
 }
 
-message_entry_editing_done(GtkCellEditable *cell_editable, gpointer user_data)
-{
-	printf ("debugged");
+message_entry_activate(GtkEntry *messageEntry, gpointer user_data){
+	const gchar *text = gtk_entry_get_text(messageEntry);
+	if (strcmp(text, "") != 0){
+		GtkLabel* messageLabel = gtk_label_new(text);
+		gtk_box_pack_start(priv->messagesBox, messageLabel, FALSE, FALSE, 0);
+		gtk_widget_set_halign(messageLabel, GTK_ALIGN_END);
+		gtk_widget_set_margin_end(messageLabel, 20);
+		gtk_widget_set_visible (messageLabel, TRUE);
+		gtk_entry_set_text (messageEntry, "");
+	}	
+	return TRUE;
+}
+
+nm_switch_activated(GtkSwitch* widget, gboolean state, gpointer user_data){
+	if (state) {
+		gtk_style_context_remove_provider_for_screen(screen, lightProvider);
+		gtk_style_context_add_provider_for_screen (screen,
+		                                GTK_STYLE_PROVIDER(darkProvider),
+		                                GTK_STYLE_PROVIDER_PRIORITY_USER);
+	}
+	else {
+		gtk_style_context_remove_provider_for_screen(screen, darkProvider);
+		gtk_style_context_add_provider_for_screen (screen,
+		                                GTK_STYLE_PROVIDER(lightProvider),
+		                                GTK_STYLE_PROVIDER_PRIORITY_USER);
+	}
+	gtk_switch_set_state (widget, state);
+	return TRUE;
+}
+
+on_modal_window_delete(GtkWidget *widget, GdkEvent  *event, gpointer user_data){
+	gtk_widget_hide(widget);
+	return TRUE;
+}
+
+on_window_destroy(GtkWindow *window){
+	cleanup();
+}
+
+settings_btn_clicked(GtkButton *button){
+	gtk_widget_show(priv->settingsDialog);
 }
 
 Gtktelegraph *
@@ -176,4 +250,3 @@ gtk_telegraph_new (void)
 	                     "flags", G_APPLICATION_HANDLES_OPEN,
 	                     NULL);
 }
-
